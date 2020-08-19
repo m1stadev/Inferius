0b712aa66bc1d245a6c5b8691c8e8f593f1f3f37
marijuanARM <37033149+marijuanARM@users.noreply.github.com>
2020-08-19 01:17:13 -0500

    some cleanup

--- inferius.py
+++ inferius.py
@@ -3,16 +3,23 @@
 import argparse
 import os
 import shutil
-from resources import extract 
+from resources import extract
+import sys
 
+assert ('darwin' in sys.platform), "This script can only be ran on macOS. Please run this on a macOS computer."
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--ipsw', help='specify IPSW')
 parser.add_argument('-r', '--ramdisk', help='find and print ramdisk name')
+parser.add_argument('-v', '--verbose', help='print verbose output', action='store_true')
 args = parser.parse_args()
 if args.ipsw:
-    print(f'extracting IPSW at this -> {args.ipsw}')
-    extract.extract_ipsw(args.ipsw)
+    if args.verbose:
+        print(f'extracting IPSW at this -> {args.ipsw}')
+        extract.extract_ipsw(args.ipsw, 'yes')
+    else:
+        print(f'extracting IPSW at this -> {args.ipsw}')
+        extract.extract_ipsw(args.ipsw)
 
 if args.ramdisk:
     extract.find_ramdisk()
--- resources/extract.py
+++ resources/extract.py
@@ -7,36 +7,44 @@ tmpdir = str(tempdir_process.stdout.read())
 tmpdir = tmpdir[2:-3]
 
 def extract_asr(ramdisk, verbose=None):
-    if verbose:
-        print(f'created temp dir at: {tmpdir}')
     ramdisk_mount = f'{tmpdir}/dmg'
     if os.path.exists(f''):
         os.makedirs(ramdisk_mount)
-    hdiutil_process = subprocess.Popen(f'/usr/bin/hdiutil attach {ramdisk} -mountpoint {ramdisk_mount}', stdout=subprocess.PIPE, shell=True)
+    
+    subprocess.Popen(f'/usr/bin/hdiutil attach {ramdisk} -mountpoint {ramdisk_mount}', stdout=subprocess.PIPE, shell=True)
     if verbose:
-        print(hdiutil_process.stdout.read())
+        print('Attaching ramdisk with hdiutil...\n')
     try:
+        if verbose:
+            print('Copying asr binary from ramdisk to work directory...\n')
         shutil.copyfile(f'{ramdisk_mount}/usr/sbin/asr', 'work/unpatched_files/asr')
     except FileNotFoundError:
-        print("asr binary not found, dmg must not be mounted! make sure you don't have any other DMGs mounted, then run the script again\nExiting...")
+        print("asr binary not found, dmg must not be mounted! Make sure you don't have any other DMGs mounted, then run the script again\nExiting...")
+        subprocess.Popen(f'/usr/bin/hdiutil detach {ramdisk_mount}', stdout=subprocess.PIPE, shell=True)
         raise
-    hdiutil_process = subprocess.Popen(f'/usr/bin/hdiutil detach {ramdisk_mount}', stdout=subprocess.PIPE, shell=True)
+        exit()
+    subprocess.Popen(f'/usr/bin/hdiutil detach {ramdisk_mount}', stdout=subprocess.PIPE, shell=True)
 
 def extract_ipsw(ipsw, verbose=None):
-    print('This may take a while and freeze your PC, please wait!')
+    print('This may take a while and freeze your PC, please wait!\n')
     shutil.unpack_archive(ipsw, f'{tmpdir}/ipsw', 'zip')
-    print(f"IPSW extracted at '{tmpdir}/ipsw'!")
     ipsw_path = f'{tmpdir}/ipsw'
-    for file in os.listdir(f'{tmpdir}/ipsw'):
-        if 'iBSS' in file:
-            ibss = file
-        if 'iBEC' in file:
-            ibec = file
+    print(f"IPSW extracted at '{ipsw_path}'!\n")
+    if verbose:
+        print('Finding ramdisk...\n')
+        restore_ramdisk = find_ramdisk(ipsw_path, 'yes')
+        print('Ramdisk found! Starting asr extraction process...')
+    else:
+        restore_ramdisk = find_ramdisk(ipsw_path)
+    
 
-def find_ramdisk(verbose=None):
+def find_ramdisk(ipsw_dir, verbose=None):
     dmg_list = []
     dmgs_list = []
-    for file in os.listdir('/Volumes/My Shit/Apple/IPSWs/iPhone6,1/10.2/10.2/'):
+    if 'BuildManifest.plist' not in os.listdir(ipsw_dir):
+        print('Error, IPSW does not exist at current directory! Exiting.')
+        raise Exception('Error, IPSW does not exist at current directory! Exiting.')
+    for file in os.listdir(ipsw_dir):
         if file.endswith('.dmg'):
             if file.startswith('._'):
                 continue
@@ -44,9 +52,9 @@ def find_ramdisk(verbose=None):
             dmgs_list.append((os.path.getsize(f'/Volumes/My Shit/Apple/IPSWs/iPhone6,1/10.2/10.2/{file}')))
 
     smallest_dmg = dmgs_list.index(min(dmgs_list))
-
+    if verbose:
+        print(f'Ramdisk found: {dmg_list[smallest_dmg]}')
     return dmg_list[smallest_dmg]
-    print(f'Ramdisk = {dmg_list[smallest_dmg]}')
 
 
 
