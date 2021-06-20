54da96fc93b3abb5fbb4158ca05c325da27a8c2e
m1stadev <37033149+m1stadev@users.noreply.github.com>
2021-06-20 12:01:59 -0500

    [api] Rewrite

--- resources/api.py
+++ resources/api.py
@@ -3,41 +3,36 @@ import requests
 import sys
 
 class API(object):
-	def __init__(self, device_identifier):
-		super().__init__()
+	def __init__(self, identifier):
+		self.check_device(identifier)
 
-		self.device = device_identifier
-
-		self.api = requests.get('https://api.ipsw.me/v2.1/firmwares.json/condensed').json()
-		self.check_device()
 		self.boardconfig = self.fetch_boardconfig()
 
-	def check_device(self):
-		if self.device not in self.api['devices']:
-			sys.exit(f'[ERROR] {self.device} does not exist. Exiting.')
+	def check_device(self, device):
+		api = requests.get(f'https://api.ipsw.me/v4/device/{device}type=ipsw')
+
+		if api.status_code != 200:
+			sys.exit(f"[ERROR] '{device}' does not exist. Exiting.")
+		
+		self.api = api.json()
+		self.device = device
 
 	def check_version(self, version):
-		if not any(self.api['devices'][self.device]['firmwares'][x]['version'] == version for x in range(len(self.api['devices'][self.device]['firmwares']))):
-			sys.exit(f'[ERROR] {version} does not exist. Exiting.')
+		if not any(firm['version'] == version for firm in self.api['firmwares']):
+			sys.exit(f"[ERROR] '{version}' does not exist. Exiting.")
 
 	def check_signing(self, version):
-		if any(self.api['devices'][self.device]['firmwares'][x]['signed'] == True for x in range(len(self.api['devices'][self.device]['firmwares'])) if self.api['devices'][self.device]['firmwares'][x]['version'] == version):
-			return True
-
-		return False
+		return any(firm['signed'] == True for firm in self.api['firmwares'] if firm['version'] == version)
 
 	def fetch_boardconfig(self):
-		device_boardconfigs = list()
-		all_boardconfigs = requests.get('https://gist.githubusercontent.com/m1stadev/6041aa45974c047b3d75da98b9926210/raw/95993516bdd086cf4b23d2771d57d0ef75bc6540/boardconfigs.json').json()
-		for x in all_boardconfigs[self.device]:
-			device_boardconfigs.append(x.lower())
+		return [board['boardconfig'] for board in self.api['boards']]
 
-		return device_boardconfigs
-
-	def fetch_sha1(self, buildid): return next(self.api['devices'][self.device]['firmwares'][x]['sha1sum'] for x in range(len(self.api['devices'][self.device]['firmwares'])) if self.api['devices'][self.device]['firmwares'][x]['buildid'] == buildid)
+	def fetch_sha1(self, buildid):
+		return next(firm['sha1sum'] for firm in self.api['firmwares'] if firm['buildid'] == buildid)
 
 	def fetch_latest(self, component, path):
-		with remotezip.RemoteZip(self.api['devices'][self.device]['firmwares'][0]['url']) as ipsw:
+		latest = self.api['firmwares'][0]
+		with remotezip.RemoteZip(latest['url']) as ipsw:
 			ipsw.extract(component, path)
 		
-		return self.api['devices'][self.device]['firmwares'][0]['version']
+		return latest['version']
