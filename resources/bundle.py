d9dd5e082bf7efda32f604b9908fe96d40b8760e
m1stadev <37033149+m1stadev@users.noreply.github.com>
2021-06-20 12:25:02 -0500

    [bundle] Rewrite

--- resources/bundle.py
+++ resources/bundle.py
@@ -9,61 +9,35 @@ class Bundle(object):
 	def __init__(self, ipsw_path):
 		self.ipsw = ipsw_path
 
-	def fetch_bundle(self, device, version, buildid):
-		bundle = requests.get(f'https://github.com/m1stadev/inferius-ext/raw/master/{device}_{version}_{buildid}_bundle.zip')
+	def fetch_bundle(self, device, version, buildid, tmpdir):
+		bundle_name = f'{device}_{version}_{buildid}_bundle'
+		bundle = requests.get(f'https://github.com/m1stadev/inferius-ext/raw/master/bundles/{bundle_name}.zip')
 		if bundle.status_code == 404:
-			sys.exit(f'[ERROR] A Firmware Bundle does not exist for device: {device}, version: {version}. Exiting.')
+			sys.exit(f'[ERROR] A Firmware Bundle does not exist for ({device}, {version}). Exiting.')
 
-		#os.makedirs(f'.tmp/Inferius/{device}_{version}_bundle')
-
-		with open(f'.tmp/Inferius/{device}_{version}_{buildid}_bundle.zip', 'wb') as f:
-			f.write(bundle.content)
-
-		with zipfile.ZipFile(f'.tmp/Inferius/{device}_{version}_{buildid}_bundle.zip', 'r') as f:
+		with zipfile.ZipFile(bundle.content, 'r') as f:
 			try:
-				f.extractall(f'.tmp/Inferius/{device}_{version}_{buildid}_bundle')
+				f.extractall(f'{tmpdir}/bundle_name')
 			except OSError:
-				sys.exit('[ERROR] Ran out of storage while extracting Firmware Bundle. Ensure you have at least 10gbs of free space on your computer, then try again. Exiting.', is_verbose)
+				sys.exit('[ERROR] Ran out of storage while extracting Firmware Bundle. Exiting.')
 
-		os.remove(f'.tmp/Inferius/{device}_{version}_{buildid}_bundle.zip')
-
-		self.bundle = f'.tmp/Inferius/{device}_{version}_{buildid}_bundle'
+		self.bundle = '/'.join((tmpdir, bundle_name))
 
 	def check_update_support(self):
 		with open(f'{self.bundle}/Info.json', 'r') as f:
 			bundle_data = json.load(f)
 
-		if 'UpdateRamdisk' in bundle_data['patches']['required']:
-			return True
-
-		return False
+		return 'UpdateRamdisk' in bundle_data['patches']['required']
 
 	def apply_patches(self):
-		patched_components = list()
-
 		with open(f'{self.bundle}/Info.json', 'r') as f:
 			bundle_data = json.load(f)
 
-		for x in bundle_data['patches']:
-			if x == 'required':
-				continue
+		for patches in bundle_data['patches']:
+			if patches != 'required':
+				apply_patch = input(f"[NOTE] Would you like to apply '{patches}' patch to your custom IPSW? [Y\\N]: ").lower()
+				if (apply_patch not in ('y', 'n')) or (apply_patch == 'n'):
+					continue
 
-			apply_patch = input(f"[NOTE] Would you like to apply '{x}' patch to your custom IPSW? [Y\\N]: ").lower()
-
-			if apply_patch not in ('y', 'n'):
-				print(f"[ERROR] Invalid input given, not applying '{x}' patch.")
-				continue
-
-			if apply_patch == 'n':
-				print(f"[ERROR] Requested not to apply '{x}' patch.")
-				continue
-
-			for i in bundle_data['patches'][x]:
-				patched_components.append(i)
-				bsdiff4.file_patch_inplace(f"{self.ipsw}/{bundle_data['patches'][x][i]['file']}", f"{self.bundle}/{bundle_data['patches'][x][i]['patch']}")
-
-		for x in bundle_data['patches']['required']:
-			if x in patched_components:
-				continue
-
-			bsdiff4.file_patch_inplace(f"{self.ipsw}/{bundle_data['patches']['required'][x]['file']}", f"{self.bundle}/{bundle_data['patches']['required'][x]['patch']}")
+			for patch in bundle_data['patches'][patches]:
+				bsdiff4.file_patch_inplace(f"{self.ipsw}/{patch['file']}", f"{self.bundle}/{patch['patch']}")
