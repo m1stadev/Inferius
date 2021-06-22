183f2eb4f258d6124cc0923288cb4910ba2e5ded
m1stadev <37033149+m1stadev@users.noreply.github.com>
2021-06-22 09:21:13 -0500

    [device] Reorganize functions

--- resources/device.py
+++ resources/device.py
@@ -15,29 +15,6 @@ class Device(object):
 		self.apnonce = self.fetch_apnonce()
 		self.ecid = self.fetch_ecid()
 
-	def get_backend(self): # Attempt to find a libusb 1.0 library to use as pyusb's backend, exit if one isn't found.
-		directories = ('/usr/lib', '/opt/procursus/lib', '/usr/local/lib') # Common library directories to search
-
-		libusb1 = None
-		for libdir in directories:
-			for file in glob.glob(libdir + '/**', recursive=True):
-				if os.path.isdir(file) or (not any(ext in file for ext in ('so', 'dylib'))):
-					continue
-
-				if 'libusb-1.0' in file:
-					libusb1 = file
-					break
-
-			else:
-				continue
-
-			break
-
-		if libusb1 is None:
-			sys.exit('[ERROR] libusb is not installed. Install libusb. Exiting.')
-
-		return usb.backend.libusb1.get_backend(find_library=lambda x:libusb1)
-
 	def check_baseband(self):
 		if self.device.startswith('iPhone'):
 			return True
@@ -76,20 +53,36 @@ class Device(object):
 		if 'PWND:' not in device.serial_number:
 			sys.exit('[ERROR] Attempting to restore a device not in Pwned DFU mode. Exiting.')
 
-	def fetch_boardconfig(self):
-		irecovery = subprocess.run(('irecovery', '-qv'), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, universal_newlines=True).stderr
-		return irecovery.splitlines(4).split(', ')[1].replace('model ', '')
-
 	def fetch_apnonce(self):
 		irecovery = subprocess.run(('irecovery', '-q'), stdout=subprocess.PIPE, universal_newlines=True).stdout
 		return irecovery.splitlines()[-3].split(' ')[-1]
 
-	def fetch_platform(self):
-		device = usb.core.find(idVendor=0x5AC, idProduct=0x1227, backend=self.backend)
-		if device is None:
-			sys.exit('[ERROR] Device in DFU mode not found. Exiting.')
+	def get_backend(self): # Attempt to find a libusb 1.0 library to use as pyusb's backend, exit if one isn't found.
+		directories = ('/usr/lib', '/opt/procursus/lib', '/usr/local/lib') # Common library directories to search
 
-		return int(device.serial_number.split(' ')[0].split(':')[1])
+		libusb1 = None
+		for libdir in directories:
+			for file in glob.glob(libdir + '/**', recursive=True):
+				if os.path.isdir(file) or (not any(ext in file for ext in ('so', 'dylib'))):
+					continue
+
+				if 'libusb-1.0' in file:
+					libusb1 = file
+					break
+
+			else:
+				continue
+
+			break
+
+		if libusb1 is None:
+			sys.exit('[ERROR] libusb is not installed. Install libusb. Exiting.')
+
+		return usb.backend.libusb1.get_backend(find_library=lambda x:libusb1)
+
+	def fetch_boardconfig(self):
+		irecovery = subprocess.run(('irecovery', '-qv'), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, universal_newlines=True).stderr
+		return irecovery.splitlines(4).split(', ')[1].replace('model ', '')
 
 	def fetch_ecid(self):
 		device = usb.core.find(idVendor=0x5AC, idProduct=0x1227, backend=self.backend)
@@ -105,3 +98,10 @@ class Device(object):
 				break
 
 		return ecid
+
+	def fetch_platform(self):
+		device = usb.core.find(idVendor=0x5AC, idProduct=0x1227, backend=self.backend)
+		if device is None:
+			sys.exit('[ERROR] Device in DFU mode not found. Exiting.')
+
+		return int(device.serial_number.split(' ')[0].split(':')[1])
