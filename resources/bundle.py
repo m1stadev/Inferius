f728266f7bc198aa9824fe3340777b75bb786804
m1stadev <37033149+m1stadev@users.noreply.github.com>
2021-06-22 09:20:49 -0500

    [bundle] Reorganize functions

--- resources/bundle.py
+++ resources/bundle.py
@@ -1,34 +1,13 @@
 import bsdiff4
 import json
-import os
 import requests
 import sys
 import zipfile
 
 
 class Bundle(object):
-	def __init__(self, ipsw_path):
-		self.ipsw = ipsw_path
-
-	def fetch_bundle(self, device, version, buildid, tmpdir):
-		bundle_name = f'{device}_{version}_{buildid}_bundle'
-		bundle = requests.get(f'https://github.com/m1stadev/inferius-ext/raw/master/bundles/{bundle_name}.zip')
-		if bundle.status_code == 404:
-			sys.exit(f'[ERROR] A Firmware Bundle does not exist for ({device}, {version}). Exiting.')
-
-		with zipfile.ZipFile(bundle.content, 'r') as f:
-			try:
-				f.extractall(f'{tmpdir}/bundle_name')
-			except OSError:
-				sys.exit('[ERROR] Ran out of storage while extracting Firmware Bundle. Exiting.')
-
-		self.bundle = '/'.join((tmpdir, bundle_name))
-
-	def check_update_support(self):
-		with open(f'{self.bundle}/Info.json', 'r') as f:
-			bundle_data = json.load(f)
-
-		return 'UpdateRamdisk' in bundle_data['patches']['required']
+	def __init__(self, ipsw):
+		self.ipsw = ipsw
 
 	def apply_patches(self):
 		with open(f'{self.bundle}/Info.json', 'r') as f:
@@ -41,4 +20,24 @@ class Bundle(object):
 					continue
 
 			for patch in bundle_data['patches'][patches]:
-				bsdiff4.file_patch_inplace(f"{self.ipsw}/{patch['file']}", f"{self.bundle}/{patch['patch']}")
+				bsdiff4.file_patch_inplace('/'.join((self.ipsw, patch['file'])), '/'.join((self.bundle, patch['file'])))
+
+	def check_update_support(self):
+		with open(f'{self.bundle}/Info.json', 'r') as f:
+			bundle_data = json.load(f)
+
+		return 'UpdateRamdisk' in bundle_data['patches']['required']
+
+	def fetch_bundle(self, device, version, buildid, tmpdir):
+		bundle_name = f'{device}_{version}_{buildid}_bundle'
+		bundle = requests.get(f'https://github.com/m1stadev/inferius-ext/raw/master/bundles/{bundle_name}.zip')
+		if bundle.status_code == 404:
+			sys.exit(f'[ERROR] A Firmware Bundle does not exist for ({device}, {version}). Exiting.')
+
+		with zipfile.ZipFile(bundle.content, 'r') as f:
+			try:
+				f.extractall('/'.join((tmpdir, bundle_name)))
+			except OSError:
+				sys.exit('[ERROR] Ran out of storage while extracting Firmware Bundle. Exiting.')
+
+		self.bundle = '/'.join((tmpdir, bundle_name))
