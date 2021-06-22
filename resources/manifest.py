edb8af10e5e1cf23906b3071aa4a4447513c0730
m1stadev <37033149+m1stadev@users.noreply.github.com>
2021-06-22 09:30:03 -0500

    [manifest] Rewrite

--- resources/manifest.py
+++ resources/manifest.py
@@ -8,28 +8,26 @@ class Manifest(object):
 		self.buildid = self.fetch_buildid()
 		self.supported_devices = self.fetch_supported_devices()
 
-	def fetch_version(self): return self.manifest['ProductVersion']
-
 	def fetch_buildid(self): return self.manifest['ProductBuildVersion']
-
-	def fetch_supported_devices(self): return self.manifest['SupportedProductTypes']
 
 	def fetch_component_path(self, boards, component):
 		return next(identity['Manifest'][component]['Info']['Path'] for identity in self.manifest['BuildIdentities'] if identity['Info']['DeviceClass'] in boards)
 
+	def fetch_supported_devices(self): return self.manifest['SupportedProductTypes']
+
+	def fetch_version(self): return self.manifest['ProductVersion']
 
 class RestoreManifest(object):
-	def __init__(self, identifier, manifest, boards):
-		self.device = identifier
-		self.boards = boards
-		self.manifest = plistlib.load(manifest)
-		self.platform = self.fetch_platform()
+	def __init__(self, manifest, boards):
+		self.platform = self.fetch_platform(boards, plistlib.load(manifest))
 
-	def fetch_platform(self):
-		for device in self.manifest['DeviceMap']:
-			if device['BoardConfig'] in self.boards:
-				if device['Platform'].startswith('s5l89'):
-					return int(device['Platform'][3:-1])
+	def fetch_platform(self, boards, manifest):
+		for device in manifest['DeviceMap']:
+			if device['BoardConfig'] not in self.boards:
+				continue
 
-				else:
-					return int(device['Platform'][-4:])
+			if device['Platform'].startswith('s5l89'):
+				return int(device['Platform'][3:-1])
+
+			else:
+				return int(device['Platform'][-4:])
