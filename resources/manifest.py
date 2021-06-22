c6cae946e79093e23a62e94189bc26ac570c187c
m1stadev <37033149+m1stadev@users.noreply.github.com>
2021-06-22 02:11:45 -0500

    [manifest] Rewrite

--- resources/manifest.py
+++ resources/manifest.py
@@ -1,9 +1,8 @@
 import plistlib
 
+
 class Manifest(object):
 	def __init__(self, manifest):
-		super().__init__()
-
 		self.manifest = plistlib.load(manifest)
 		self.version = self.fetch_version()
 		self.buildid = self.fetch_buildid()
@@ -15,23 +14,22 @@ class Manifest(object):
 
 	def fetch_supported_devices(self): return self.manifest['SupportedProductTypes']
 
-	def fetch_component_path(self, boardconfig, component): return next(self.manifest['BuildIdentities'][x]['Manifest'][component]['Info']['Path'] for x in range(len(self.manifest['BuildIdentities'])) if self.manifest['BuildIdentities'][x]['Info']['DeviceClass'] in boardconfig)
+	def fetch_component_path(self, boards, component):
+		return next(identity['Manifest'][component]['Info']['Path'] for identity in self.manifest['BuildIdentities'] if identity['Info']['DeviceClass'] in boards)
+
 
 class RestoreManifest(object):
-	def __init__(self, manifest, device, boardconfig):
-		super().__init__()
-
-		self.device = device
-		self.boardconfig = boardconfig
+	def __init__(self, identifier, manifest, boards):
+		self.device = identifier
+		self.boards = boards
 		self.manifest = plistlib.load(manifest)
 		self.platform = self.fetch_platform()
 
 	def fetch_platform(self):
-		for x in range(len(self.manifest['DeviceMap'])):
-			if self.manifest['DeviceMap'][x]['BoardConfig'] in self.boardconfig:
-
-				if self.manifest['DeviceMap'][x]['Platform'].startswith('s5l89'):
-					return int(self.manifest['DeviceMap'][x]['Platform'][3:-1])
+		for device in self.manifest['DeviceMap']:
+			if device['BoardConfig'] in self.boards:
+				if device['Platform'].startswith('s5l89'):
+					return int(device['Platform'][3:-1])
 
 				else:
-					return int(self.manifest['DeviceMap'][x]['Platform'][-4:])
+					return int(device['Platform'][-4:])
