1a9a1ccd10eddc209a338ef29e2ed40bf08b0005
m1stadev <adamhamdi31@gmail.com>
2022-01-06 22:36:07 -0600

    [api] Rewrite - Actually use exceptions - Type-hinting - Change return of some functions

--- utils/api.py
+++ utils/api.py
@@ -1,59 +1,71 @@
+from pathlib import Path
+from typing import Optional
+from utils import errors
+
 import remotezip
 import requests
-import sys
 
 
 class API:
-    def check_device(self, identifier):
-        api = requests.get('https://api.ipsw.me/v4/devices').json()
-
-        if identifier not in [device['identifier'] for device in api]:
-            sys.exit(f"[ERROR] '{identifier}' does not exist. Exiting.")
-
+    def __init__(self, identifier: str) -> None:
         self.device = identifier
+        self.api = self.fetch_api()
+        self.board = self.get_board()
 
-    def is_signed(self, version):
-        return any(firm['signed'] == True for firm in self.api['firmwares'] if firm['version'] == version)
+    def is_signed(self, version: str) -> bool: return any(firm['signed'] == True for firm in self.api['firmwares'] if firm['version'] == version)
 
-    def check_version(self, version):
+    def check_version(self, version) -> Optional[bool]:
         if not any(firm['version'] == version for firm in self.api['firmwares']):
-            sys.exit(f"[ERROR] '{version}' does not exist. Exiting.")
+            raise errors.NotFoundError(f"Version does not exist: {version}.")
 
-    def fetch_api(self, identifier=None):
-        self.api = requests.get(f'https://api.ipsw.me/v4/device/{identifier if identifier else self.device}?type=ipsw').json()
+        return True
 
-    def fetch_sha1(self, buildid):
+    def fetch_api(self) -> Optional[dict]:
+        try:
+            return requests.get(f'https://api.ipsw.me/v4/device/{self.device}').json()
+        except requests.exceptions.JSONDecodeError:
+            raise errors.NotFoundError(f"Device does not exist: {self.device}.")
+
+    def fetch_board(self) -> Optional[str]:
+        boards = [board['boardconfig'].lower() for board in self.api['boards'] if board['boardconfig'].lower().endswith('ap')]
+        if len(boards) == 1:
+            return boards[0]
+
+        else:
+            print('There are multiple board configs for your device! Please choose the correct boardc onfig for your device:')
+            for b in range(len(boards)):
+                print(f"  {b + 1}: {boards[b]}")
+
+            board = input('Choice: ')
+            try:
+                board = int(board) - 1
+            except:
+                raise TypeError(f'Invalid choice given: {board}.')
+            else:
+                if board not in range(len(boards)):
+                    raise ValueError(f'Incorrect choice given: {board}.')
+
+            return boards[board]
+
+    def fetch_sha1(self, buildid: str) -> str:
         return next(firm['sha1sum'] for firm in self.api['firmwares'] if firm['buildid'] == buildid)
 
-    def get_board(self):
-        boards = [board['boardconfig'] for board in self.api['boards']]
-        if len(boards) == 1:
-            self.board = boards[0]
-            return
-        
-        print('There are multiple boardconfigs for your device! Please choose the correct boardconfig for your device:')
-        for board in range(len(boards)):
-            print(f"  {board + 1}: {boards[board]}")
-
-        board = input('Choice: ')
-        try:
-            board = int(board) - 1
-        except:
-            sys.exit('[ERROR] Invalid input given. Exiting.')
-        else:
-            if board not in range(len(boards)):
-                sys.exit('[ERROR] Invalid input given. Exiting.')
-
-        self.board = boards[board]
-
-    def partialzip_extract(self, buildid, component, path):
+    def partialzip_extract(self, buildid: str, component: str, path: Path) -> Path:
         firm = next(firm for firm in self.api['firmwares'] if firm['buildid'] == buildid)
+
         with remotezip.RemoteZip(firm['url']) as ipsw:
             ipsw.extract(component, path)
 
-        return firm['version']
+        return path / Path(component)
 
-    def partialzip_read(self, buildid, component):
-        firm = next(firm for firm in self.api['firmwares'] if firm['buildid'] == buildid)
+    def partialzip_read(self, buildid: str, component: str) -> bytes:
+        try:
+            firm = next(firm for firm in self.api['firmwares'] if firm['buildid'] == buildid)
+        except StopIteration:
+            raise errors.NotFoundError(f'Buildid does not exist: {buildid}.')
+
         with remotezip.RemoteZip(firm['url']) as ipsw:
-            return ipsw.read(component)
+            try:
+                return ipsw.read(component)
+            except KeyError:
+                raise errors.NotFoundError(f'Component does not exist: {component}.')
