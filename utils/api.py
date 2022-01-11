88bf0033289e1aae2e16154369751fc9afb88e88
m1stadev <adamhamdi31@gmail.com>
2022-01-10 22:27:13 -0600

    [API] Cleanup - More error handling - Add errors.IOError - Code cleanup

--- utils/api.py
+++ utils/api.py
@@ -14,12 +14,6 @@ class API:
 
     def is_signed(self, version: str) -> bool: return any(firm['signed'] == True for firm in self.api['firmwares'] if firm['version'] == version)
 
-    def check_version(self, version) -> Optional[bool]:
-        if not any(firm['version'] == version for firm in self.api['firmwares']):
-            raise errors.NotFoundError(f"Version does not exist: {version}.")
-
-        return True
-
     def fetch_api(self) -> Optional[dict]:
         try:
             return requests.get(f'https://api.ipsw.me/v4/device/{self.device}').json()
@@ -32,7 +26,7 @@ class API:
             return boards[0]
 
         else:
-            print('There are multiple board configs for your device! Please choose the correct boardc onfig for your device:')
+            print('There are multiple board configs for your device! Please choose the correct board config for your device:')
             for b in range(len(boards)):
                 print(f"  {b + 1}: {boards[b]}")
 
@@ -48,32 +42,37 @@ class API:
             return boards[board]
 
     def fetch_sha1(self, buildid: str) -> str:
-        return next(firm['sha1sum'] for firm in self.api['firmwares'] if firm['buildid'] == buildid)
+        try:
+            sha1 = next(firm['sha1sum'] for firm in self.api['firmwares'] if firm['buildid'] == buildid)
+        except StopIteration:
+            raise errors.NotFoundError(f'Firmware does not exist with buildid: {buildid}.')
+
+        return sha1
 
     def partialzip_extract(self, buildid: str, component: str, path: Path) -> Optional[Path]:
         try:
-            firm = next(firm for firm in self.api['firmwares'] if firm['buildid'] == buildid)
+            url = next(firm['url'] for firm in self.api['firmwares'] if firm['buildid'] == buildid)
         except StopIteration:
-            raise errors.NotFoundError(f'Buildid does not exist: {buildid}.')
+            raise errors.NotFoundError(f'Firmware does not exist with buildid: {buildid}.')
 
-        with RemoteZip(firm['url']) as ipsw:
+        with RemoteZip(url) as ipsw:
             try:
                 ipsw.extract(component, path)
             except KeyError:
                 raise errors.NotFoundError(f'Component does not exist: {component}.')
             except OSError:
-                raise OSError(f'Failed to partialzip component: {component}.')
+                raise errors.IOError(f'Failed to partialzip component: {component}.')
 
         return path / component
 
     def partialzip_read(self, buildid: str, component: str) -> Optional[bytes]:
         try:
-            firm = next(firm for firm in self.api['firmwares'] if firm['buildid'] == buildid)
+            url = next(firm['url'] for firm in self.api['firmwares'] if firm['buildid'] == buildid)
         except StopIteration:
-            raise errors.NotFoundError(f'Buildid does not exist: {buildid}.')
+            raise errors.NotFoundError(f'Firmware does not exist with buildid: {buildid}.')
 
-        with RemoteZip(firm['url']) as ipsw:
+        with RemoteZip(url) as ipsw:
             try:
                 return ipsw.read(component)
             except KeyError:
-                raise errors.NotFoundError(f'Component does not exist: {component}.')
+                raise errors.NotFoundError(f'File does not exist in IPSW: {component}.')
--- utils/errors.py
+++ utils/errors.py
@@ -1,14 +1,22 @@
 class InferiusError(Exception):
     pass
 
+
 class DependencyError(InferiusError):
     pass
+
 
 class NotFoundError(InferiusError):
     pass
 
+
 class InvalidChoiceError(InferiusError, ValueError):
     pass
 
+
 class InvalidTypeError(InferiusError, TypeError):
+    pass
+
+
+class IOError(InferiusError, OSError):
     pass
