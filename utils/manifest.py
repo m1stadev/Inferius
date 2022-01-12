03b60aa100c8c5b07a80911157449346cafa4002
m1stadev <adamhamdi31@gmail.com>
2022-01-11 23:55:55 -0600

    [manifest] Cleanup - Type-hinting - RestoreManifest.platform is now a property function - Formatting

--- utils/manifest.py
+++ utils/manifest.py
@@ -1,24 +1,32 @@
+from typing import Optional
+
 import plistlib
 
 
 class Manifest:
-    def __init__(self, manifest):
-        self.manifest = plistlib.loads(manifest)
-        self.version = (int(_) for _ in self.manifest['ProductVersion'].split('.'))
-        self.buildid = self.manifest['ProductBuildVersion']
-        self.supported_devices = self.manifest['SupportedProductTypes']
+    def __init__(self, manifest: bytes):
+        self._manifest = plistlib.loads(manifest)
+        self.version = (int(_) for _ in self._manifest['ProductVersion'].split('.'))
+        self.buildid = self._manifest['ProductBuildVersion']
+        self.supported_devices = self._manifest['SupportedProductTypes']
 
     def fetch_component_path(self, boardconfig: str, component: str) -> str:
-        return next(identity['Manifest'][component]['Info']['Path'] for identity in self.manifest['BuildIdentities'] if identity['Info']['DeviceClass'].lower() == boardconfig.lower())
+        return next(
+            identity['Manifest'][component]['Info']['Path']
+            for identity in self.manifest['BuildIdentities']
+            if identity['Info']['DeviceClass'].lower() == boardconfig.lower()
+        )
 
 
 class RestoreManifest:
-    def __init__(self, manifest, boardconfig):
-        self.platform = self.fetch_platform(boardconfig, plistlib.loads(manifest))
+    def __init__(self, manifest: bytes, boardconfig: str):
+        self._manifest = plistlib.loads(manifest)
+        self.boardconfig = boardconfig
 
-    def fetch_platform(self, boardconfig, manifest):
-        for device in manifest['DeviceMap']:
-            if device['BoardConfig'].lower() != boardconfig.lower():
+    @property
+    def platform(self) -> Optional[int]:
+        for device in self._manifest['DeviceMap']:
+            if device['BoardConfig'].lower() != self.boardconfig.lower():
                 continue
 
             if device['Platform'].startswith('s5l89'):
