ae5fe93e842513be26d9b70e622a96afbce3b2de
m1stadev <adamhamdi31@gmail.com>
2022-02-05 13:26:55 -0600

    [manifest] Update - Store boardconfig as instance attribute - Add dump function for returning manifest as bytes - get_path: Update with error handling

--- utils/manifest.py
+++ utils/manifest.py
@@ -1,21 +1,45 @@
 from typing import Optional
+from utils import errors
 
 import plistlib
 
 
 class Manifest:
-    def __init__(self, manifest: bytes):
+    def __init__(self, manifest: bytes, board: str):
         self._manifest = plistlib.loads(manifest)
-        self.version = tuple(int(_) for _ in self._manifest['ProductVersion'].split('.'))
+
+        self.version = tuple(
+            int(_) for _ in self._manifest['ProductVersion'].split('.')
+        )
         self.buildid = self._manifest['ProductBuildVersion']
         self.supported_devices = self._manifest['SupportedProductTypes']
 
-    def get_path(self, boardconfig: str, component: str) -> str:
-        return next(
-            identity['Manifest'][component]['Info']['Path']
-            for identity in self._manifest['BuildIdentities']
-            if identity['Info']['DeviceClass'].lower() == boardconfig.lower()
+        # Get proper capitalization for board
+        self.board = next(
+            _['Info']['DeviceClass']
+            for _ in self._manifest['BuildIdentities']
+            if _['Info']['DeviceClass'].lower() == board.lower()
         )
+
+        self.id = next(
+            _
+            for _ in range(len(self._manifest['BuildIdentities']))
+            if self._manifest['BuildIdentities'][_]['Info']['DeviceClass'] == self.board
+        )
+
+    def get_path(self, component: str) -> str:
+        if (
+            component
+            not in self._manifest['BuildIdentities'][self.id]['Manifest'].keys()
+        ):
+            raise errors.NotFoundError(f'Component not found in manifest: {component}.')
+
+        return self._manifest['BuildIdentities'][self.id]['Manifest'][component][
+            'Info'
+        ]['Path']
+
+    def dump(self) -> bytes:
+        return plistlib.dumps(self._manifest)
 
 
 class RestoreManifest:
