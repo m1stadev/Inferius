dcb8af19ea5a1d3a69a9be537c8c9e11e26011db
m1stadev <adamhamdi31@gmail.com>
2022-02-06 00:30:01 -0600

    [restore] Update - Removed send_bootchain, _irecv_send_file, and _irecv_send_cmd - sign_component -> sign_image - sign_image: Add support for setting tag - restore: Now require ramdisk + kernelcache images to be passed - restore: Use path object for logfile - restore: Now use futurerestore to enter pwnrecovery - save_blobs: Better error handling

--- utils/restore.py
+++ utils/restore.py
@@ -1,36 +1,38 @@
 from pathlib import Path
 from utils.device import Device
-from utils import errors, usb
+from utils import errors
 
 import shutil
 import subprocess
-import time
 
 
 class Restore:
     def __init__(self, device: Device):
         self.device = device
 
-    def _irecv_send_file(self, file: Path) -> None:
-        try:
-            subprocess.check_call(
-                ('irecovery', '-f', str(file)), stdout=subprocess.DEVNULL
-            )
-        except:
-            raise errors.RestoreError(
-                f"Failed to send file to device: {file}."
-            ) from None
-
-    def _irecv_send_cmd(self, cmd: str) -> None:
-        try:
-            subprocess.check_call(
-                ('irecovery', '-c', cmd), stdout=subprocess.DEVNULL
-            )
-        except:
-            raise errors.RestoreError(f"Failed to send command to device: '{cmd}'.")
-
-    def restore(self, ipsw: Path, cellular: bool, update: bool, *, sep: Path=None, manifest: Path=None) -> None:
-        args = ['futurerestore', '-d', '-t', str(self.blob)]
+    def restore(
+        self,
+        ipsw: Path,
+        rdsk: Path,
+        kern: Path,
+        cellular: bool,
+        update: bool,
+        *,
+        sep: Path = None,
+        manifest: Path = None,
+    ) -> None:
+        args = [
+            'futurerestore',
+            '-t',
+            str(self.signing_blob),
+            '--use-pwndfu',
+            '--no-cache',
+            '--skip-blob',
+            '--rdsk',
+            str(rdsk),
+            '--rkrn',
+            str(kern),
+        ]
 
         if sep and manifest:
             args.append('-s')
@@ -52,40 +54,56 @@ class Restore:
 
         args.append(str(ipsw))
 
-        Path('restore.log').unlink(missing_ok=True)
+        log = Path('restore.log')
+        log.unlink(missing_ok=True)
+
         try:
             futurerestore = subprocess.check_output(
-                args, stderr=subprocess.STDOUT, universal_newlines=True
+                args,
+                stderr=subprocess.STDOUT,
+                universal_newlines=True,
             )
 
         except subprocess.CalledProcessError as process:
-            with open('restore.log', 'w') as f:
-                f.write(f"{' '.join([str(_) for _ in args])}\n\n")
+            with log.open('w') as f:
+                f.write(f"{' '.join([_ for _ in args])}\n\n")
                 f.write(process.stdout)
 
             raise errors.RestoreError(
-                "[ERROR] Restore failed. Log written to 'restore.log'. Exiting."
+                f"[ERROR] Restore failed. Log written to '{log.stem}'. Exiting."
             ) from None
 
         if Path(ipsw.stem).is_dir():
             shutil.rmtree(ipsw.stem)
 
         if 'Done: restoring succeeded!' not in futurerestore:
-            with open('restore.log', 'w') as f:
-                f.write(f"{' '.join([str(_) for _ in args])}\n\n")
+            with log.open('w') as f:
+                f.write(f"{' '.join([_ for _ in args])}\n\n")
                 f.write(futurerestore)
 
             raise errors.RestoreError(
-                "[ERROR] Restore failed. Log written to 'restore.log'. Exiting."
+                f"[ERROR] Restore failed. Log written to '{log.stem}'. Exiting."
             )
 
-    def save_blobs(self, ecid: str, boardconfig: str, path: Path, *, manifest: Path=None, apnonce: str=None) -> None:
+    def save_blobs(
+        self,
+        ecid: str,
+        boardconfig: str,
+        path: Path,
+        *,
+        manifest: Path = None,
+        apnonce: str = None,
+    ) -> None:
         args = [
             'tsschecker',
-            '-d', self.device.identifier,
-            '-B', boardconfig,
-            '-e', ecid,
-            '--save-path', str(path),
+            '-d',
+            self.device.identifier,
+            '-B',
+            boardconfig,
+            '-e',
+            ecid,
+            '--save-path',
+            str(path),
             '-s',
         ]
 
@@ -100,9 +118,13 @@ class Restore:
             args.append('--apnonce')
             args.append(apnonce)
 
-        tsschecker = subprocess.check_output(args, universal_newlines=True)
-        if 'Saved shsh blobs!' not in tsschecker:
-            raise errors.RestoreError('Failed to save SHSH blobs.')
+        try:
+            if 'Saved shsh blobs!' not in subprocess.check_output(
+                args, stderr=subprocess.STDOUT, universal_newlines=True
+            ):
+                raise errors.RestoreError('Failed to save SHSH blobs.')
+        except subprocess.CalledProcessError:
+            raise errors.RestoreError('Failed to save SHSH blobs.') from None
 
         if apnonce:
             for blob in path.glob('*.shsh*'):
@@ -112,24 +134,23 @@ class Restore:
         else:
             self.signing_blob = tuple(path.glob('*.shsh*'))[0]
 
-    def send_bootchain(self, ibss: Path, ibec: Path) -> None:
-        # Reset device
-        device = usb.get_device(usb.DFU)
-        usb.reset_device(device)
-        usb.release_device(device)
+    def sign_image(self, image: Path, output: Path, tag: str = None) -> None:
+        args = [
+            'img4tool',
+            '-c',
+            str(output),
+            '-p',
+            str(image),
+            '-s',
+            str(self.signing_blob),
+        ]
 
-        self._irecv_send_file(ibss)
-        self._irecv_send_file(ibec)
+        if tag:
+            if len(tag) != 4:
+                raise errors.RestoreError(f'Invalid IMG4 tag: {tag}.')
 
-        if 0x8010 <= self.device.data['CPID']:
-            device = usb.get_device(usb.RECOVERY)
-            usb.send_cmd('go')
-            usb.release_device(device)
-
-            time.sleep(3)
-
-    def sign_component(self, file: Path, output: Path) -> None:
-        args = ('img4tool', '-c', output, '-p', file, '-s', self.signing_blob)
+            args.append('-t')
+            args.append(tag.lower())
 
         try:
             subprocess.check_call(args, stdout=subprocess.DEVNULL)
